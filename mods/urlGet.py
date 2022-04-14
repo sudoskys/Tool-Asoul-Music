e194549d9cf30a0707059d721e975eaf75b0865d
‘sudoskys‘ <coldlando@hotmail.com>
2022-04-14 03:13:38 +0000

    add readme

--- mods/urlGet.py
+++ mods/urlGet.py
@@ -6,12 +6,28 @@ import requests
 class infoGet(object):
     def __init__(self):
         self.debug = False
+        self.header= {
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+            'Accept-Encoding': 'gzip, deflate',
+            'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
+            'Cache-Control': 'max-age=0',
+            'DNT': '1',
+            'Referer': 'https://api.bilibili.com/',
+            'Connection': 'keep-alive',
+            'Host': 'api.bilibili.com',
+            'Upgrade-Insecure-Requests': '1',
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:96.0) Gecko/20100101 Firefox/96.0',
+            'Cookie': '1P_JAR=2022-02-09-02;SEARCH_SAMESITE=CgQIv5QB;ID=CgQIsv5QB0',
+        }
 
     def getCidAndTitle(self, bvid, p=1):
         url = 'https://api.bilibili.com/x/web-interface/view?bvid=' + bvid
-        data = requests.get(url).json()['data']
-        title = data['title']
-        cid = data['pages'][p - 1]['cid']
+        ciddata = requests.get(url=url, headers=self.header).json().get("data")
+        if not ciddata:
+            raise Exception("Cid Api 访问异常... Detail:" + str(ciddata))
+        #data = requests.get(url).json()['data']
+        title = ciddata['title']
+        cid = ciddata['pages'][p - 1]['cid']
         return str(cid), title
 
     def getInformation(self, bvList):
