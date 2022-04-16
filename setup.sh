46ac7ea31259cfd6ad0ac08301e18883d50e54dd
‘sudoskys‘ <coldlando@hotmail.com>
2022-04-16 03:37:41 +0000

    fix:make readme clear

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 git clone https://github.com/sudoskys/Tool-Asoul-Music
 cd Tool-Asoul-Music
 echo '============NOTICE================='
@@ -5,3 +7,19 @@ echo 'you need edit the config.yaml!'
 echo '==================================='
 cat config.yaml
 echo '==================================='
+
+read -r -p "Install package from requirements.txt? [Y/n] " input
+
+case $input in
+    [yY][eE][sS]|[yY])
+		python3 -m pip install -r requirements.txt
+		;;
+
+    [nN][oO]|[nN])
+		exit 1
+        ;;
+    *)
+		echo "Invalid input..."
+		exit 1
+		;;
+esac
